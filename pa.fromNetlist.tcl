3f2131381b2ccfb7fd34c2a5908cf527187edd7d
KYHSGeekCode <jourhyang123@gmail.com>
2021-06-16 00:59:38 +0900

    :new: mul

--- pa.fromNetlist.tcl
+++ pa.fromNetlist.tcl
@@ -1,7 +1,7 @@
 
 # PlanAhead Launch Script for Post-Synthesis pin planning, created by Project Navigator
 
-create_project -name Final_home -dir "/csehome/kygc/verilog/planAhead_run_4" -part xc3s50antqg144-4
+create_project -name Final_home -dir "/csehome/kygc/verilog/planAhead_run_5" -part xc3s50antqg144-4
 set_property design_mode GateLvl [get_property srcset [current_run -impl]]
 set_property edif_top_file "/csehome/kygc/verilog/testtest.ngc" [ get_property srcset [ current_run ] ]
 add_files -norecurse { {/csehome/kygc/verilog} }
