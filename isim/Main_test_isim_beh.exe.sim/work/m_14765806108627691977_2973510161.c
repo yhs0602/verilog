6a02bf0dcbd3ca2d649f97ac841a8c110f2c7320
양현서 <kygc@my-debian2>
2021-06-15 18:50:48 +0900

    :bug: async reset

--- isim/Main_test_isim_beh.exe.sim/work/m_14765806108627691977_2973510161.c
+++ isim/Main_test_isim_beh.exe.sim/work/m_14765806108627691977_2973510161.c
@@ -26,7 +26,7 @@ static unsigned int ng1[] = {1U, 0U};
 
 
 
-static void Cont_55_0(char *t0)
+static void Cont_56_0(char *t0)
 {
     char *t1;
     char *t2;
@@ -45,17 +45,17 @@ static void Cont_55_0(char *t0)
     unsigned int t15;
     char *t16;
 
-LAB0:    t1 = (t0 + 7000U);
+LAB0:    t1 = (t0 + 7160U);
     t2 = *((char **)t1);
     if (t2 == 0)
         goto LAB2;
 
 LAB3:    goto *t2;
 
-LAB2:    xsi_set_current_line(55, ng0);
-    t2 = (t0 + 5368U);
+LAB2:    xsi_set_current_line(56, ng0);
+    t2 = (t0 + 5528U);
     t3 = *((char **)t2);
-    t2 = (t0 + 7648);
+    t2 = (t0 + 8072);
     t4 = (t2 + 56U);
     t5 = *((char **)t4);
     t6 = (t5 + 56U);
@@ -74,7 +74,61 @@ LAB2:    xsi_set_current_line(55, ng0);
     t15 = *((unsigned int *)t13);
     *((unsigned int *)t13) = (t15 | t9);
     xsi_driver_vfirst_trans(t2, 0, 7);
-    t16 = (t0 + 7568);
+    t16 = (t0 + 7976);
+    *((int *)t16) = 1;
+
+LAB1:    return;
+}
+
+static void Cont_57_1(char *t0)
+{
+    char *t1;
+    char *t2;
+    char *t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    unsigned int t8;
+    unsigned int t9;
+    char *t10;
+    unsigned int t11;
+    unsigned int t12;
+    char *t13;
+    unsigned int t14;
+    unsigned int t15;
+    char *t16;
+
+LAB0:    t1 = (t0 + 7408U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    xsi_set_current_line(57, ng0);
+    t2 = (t0 + 2328U);
+    t3 = *((char **)t2);
+    t2 = (t0 + 8136);
+    t4 = (t2 + 56U);
+    t5 = *((char **)t4);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    memset(t7, 0, 8);
+    t8 = 1U;
+    t9 = t8;
+    t10 = (t3 + 4);
+    t11 = *((unsigned int *)t3);
+    t8 = (t8 & t11);
+    t12 = *((unsigned int *)t10);
+    t9 = (t9 & t12);
+    t13 = (t7 + 4);
+    t14 = *((unsigned int *)t7);
+    *((unsigned int *)t7) = (t14 | t8);
+    t15 = *((unsigned int *)t13);
+    *((unsigned int *)t13) = (t15 | t9);
+    xsi_driver_vfirst_trans(t2, 0, 0);
+    t16 = (t0 + 7992);
     *((int *)t16) = 1;
 
 LAB1:    return;
@@ -98,7 +152,7 @@ static void implSig1_execute(char *t0)
     unsigned int t14;
     unsigned int t15;
 
-LAB0:    t1 = (t0 + 7248U);
+LAB0:    t1 = (t0 + 7656U);
     t2 = *((char **)t1);
     if (t2 == 0)
         goto LAB2;
@@ -106,7 +160,7 @@ LAB0:    t1 = (t0 + 7248U);
 LAB3:    goto *t2;
 
 LAB2:    t2 = ((char*)((ng1)));
-    t3 = (t0 + 7712);
+    t3 = (t0 + 8200);
     t4 = (t3 + 56U);
     t5 = *((char **)t4);
     t6 = (t5 + 56U);
@@ -130,9 +184,9 @@ LAB1:    return;
 }
 
 
-extern void work_m_17305226079700728452_2973510161_init()
+extern void work_m_14765806108627691977_2973510161_init()
 {
-	static char *pe[] = {(void *)Cont_55_0,(void *)implSig1_execute};
-	xsi_register_didat("work_m_17305226079700728452_2973510161", "isim/Main_test_isim_beh.exe.sim/work/m_17305226079700728452_2973510161.didat");
+	static char *pe[] = {(void *)Cont_56_0,(void *)Cont_57_1,(void *)implSig1_execute};
+	xsi_register_didat("work_m_14765806108627691977_2973510161", "isim/Main_test_isim_beh.exe.sim/work/m_14765806108627691977_2973510161.didat");
 	xsi_register_executes(pe);
 }
